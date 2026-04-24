e09f401a513eeac0df922729a217ac97cc34c8fe
Mohamednour426 <mohamed.nour426@icloud.com>
2026-04-25 00:11:21 +0300

    POS

---
+++ pos_pin_restriction/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'POS PIN Dashboard Restriction',
+    'version': '19.0.1.0.0',
+    'category': 'Sales/Point of Sale',
+    'summary': 'Restrict POS Dashboard access using Employee PIN',
+    'description': 'Restrict POS Dashboard access using Employee PIN with a secure PIN entry screen.',
+    'author': 'Antigravity',
+    'depends': ['point_of_sale', 'hr', 'pos_hr'],
+    'data': [
+        'views/actions.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'pos_pin_restriction/static/src/css/pin_screen.css',
+            'pos_pin_restriction/static/src/js/pin_screen.js',
+            'pos_pin_restriction/static/src/xml/pin_screen.xml',
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
