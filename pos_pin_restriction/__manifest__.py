af88702654dd265becc6a1fd1f0ef38c95b05f36
Mohamednour426 <mohamed.nour426@icloud.com>
2026-04-25 00:43:55 +0300

    POS

--- pos_pin_restriction/__manifest__.py
+++ pos_pin_restriction/__manifest__.py
@@ -5,6 +5,8 @@
     'summary': 'Restrict POS Dashboard access using Employee PIN',
     'description': 'Restrict POS Dashboard access using Employee PIN with a secure PIN entry screen.',
     'author': 'Mohamed Nour',
+    'price': 10.0,
+    'currency': 'USD',
     'images': [
         'static/description/thumbnail.png',
         'static/description/banner.png',
